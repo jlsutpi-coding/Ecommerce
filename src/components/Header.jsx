6a376749da788abbf3721e8c047e3f1a392b3ecd
Jl Sut Pi <sutpi070@gmail.com>
2026-03-25 23:05:39 +0700

    feat: add simple header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { LiaShoppingBagSolid } from "react-icons/lia";
+
+const Header = () => {
+  return (
+    <nav className="  h-16 px-8  bg-[#F8F9FA] flex justify-between items-center">
+      <div>
+        <h1 className=" font-extrabold leading-8 tracking-[-1.2px]  text-[24px]">
+          Indigo
+        </h1>
+      </div>
+      <div className=" flex justify-around">
+        <div>
+          <input type="text" placeholder="search" />
+        </div>
+        <button className=" relative">
+          <LiaShoppingBagSolid className="w-7 h-7" />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
