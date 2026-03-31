4999a8e543adfda10feca5cdd098dc3eb2088be2
Jl Sut Pi <sutpi070@gmail.com>
2026-04-01 00:16:59 +0700

    feat: finish product detail view and price calculations

--- src/pages/Product/ProductPage.jsx
+++ src/pages/Product/ProductPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { MdOutlineDeliveryDining } from "react-icons/md";
+import { MdOutlineDeliveryDining, MdVerified } from "react-icons/md";
 import { CiHeart } from "react-icons/ci";
 
 import {
@@ -31,62 +31,115 @@ const ProductPage = () => {
 
   if (!selectedProduct) return null;
 
-  const { category, title, price, thumbnail, rating, description } =
-    selectedProduct;
+  const {
+    category,
+    title,
+    price,
+    thumbnail,
+    rating,
+    description,
+    reviews,
+    images,
+    discountPercentage,
+    warrantyInformation,
+    shippingInformation,
+  } = selectedProduct;
+
+  const discountAmount = Math.round(price * discountPercentage) / 100;
+  const finalPrice = price - discountAmount;
 
   return (
-    <div className=" mt-24 mb-12 mx-12 flex gap-16 items-center">
-      <div className=" w-166 h-207.5 rounded-xl">
-        <img src={thumbnail} className=" w-full h-full object-contain" alt="" />
-      </div>
-      <div className=" w-114 flex flex-col gap-4 ">
-        <div className="flex flex-col gap-2 pb-6 ">
-          <p className="font-semibold text-[12px] text-primary  font-inter leading-4 tracking-[2.4px]">
-            {category?.toUpperCase()}
-          </p>
-          <h2 className="text-start font-manrope font-extrabold text-[48px] leading-12 tracking-[-1.2px] text-[#191C1D]">
-            {title}
-          </h2>
-          <div className=" flex items-center gap-4">
-            <StarRating rating={rating} />
-            <div className=" text-[#C5C5D4]/30">|</div>
-            <div className=" font-inter font-normal leading-5 text-[14px] text-[#757684]">
-              {rating?.count} Reviews
+    <div className="pt-32 pb-20 max-w-screen-2xl mx-auto px-8">
+      <div className=" mt-24 mb-12 mx-12 grid grid-cols-1 lg:grid-cols-12 gap-16">
+        {/* Left : Image Gallery (Bento style) */}
+        <div className="lg:col-span-6 grid grid-cols-6 gap-4">
+          <div className="col-span-6  rounded-xl overflow-hidden bg-surface-container-low">
+            <img
+              src={thumbnail}
+              className="w-full h-full mx-auto object-contain"
+              alt=""
+            />
+          </div>
+          {images?.map((item, index) => (
+            <div
+              key={index}
+              className="col-span-2 aspect-square rounded-xl overflow-hidden bg-surface-container-low"
+            >
+              <img className="w-full h-full object-cover" src={item} alt="" />
             </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-2 mb-8">
-          <p className=" text-[30px] font-manrope font-bold leading-9 text-primary">
-            ${price}
-          </p>
-          <div className=" flex  items-center font-inter">
-            <MdOutlineDeliveryDining className="text-[#6C3400] w-10 h-5" />
-            <p className="font-medium text-[14px] leading-5 font-manrope  text-[#6C3400]">
-              Complimentary Standard Shipping
-            </p>
-          </div>
-        </div>
-        <div className=" flex flex-col gap-4 ">
-          <button className=" shadow-xl hover:shadow-2xl active:scale-95 transition-all w-114 bg-primary py-5 px-8 leading-7 tracking-[0.45px] rounded-lg font-manrope text-white font-bold text-[18px] ">
-            Add To Cart
-          </button>
-          <button className=" hover:bg-[#f3f4f5] transition-colors duration-200 flex items-center justify-center border-[#C5C5D4]/20 border font-manrope gap-3 w-114  py-5  leading-7 tracking-[0.45px] rounded-lg   font-bold text-[18px] ">
-            <CiHeart className=" w-5  text-[#191C1D]" />
-            <span className="font-bold text-[18px] text-[#191C1D] leading-7">
-              Wishlist
-            </span>
-          </button>
+          ))}
         </div>
 
-        <div className=" mb-12  w-114 flex flex-col gap-8 pt-8">
-          <div className="">
-            <span className="  text-primary font-inter text-[14px] leading-5 tracking-[1.4px]">
-              DESCRIPTION
-            </span>
+        {/* Right Product Information */}
+        <div className="lg:col-span-5 flex flex-col gap-4 ">
+          <div className="flex flex-col gap-2 pb-6 ">
+            <p className="font-semibold uppercase text-[12px] text-primary  font-inter leading-4 tracking-[2.4px]">
+              {category}
+            </p>
+            <h2 className="text-start font-manrope font-extrabold text-[48px] leading-12 tracking-[-1.2px] text-[#191C1D]">
+              {title}
+            </h2>
+            <div className=" flex items-center gap-4">
+              <StarRating rating={rating} />
+              <div className=" text-[#C5C5D4]/30">|</div>
+              <div className=" font-inter font-normal leading-5 text-[14px] text-[#757684]">
+                {reviews.length} Reviews
+              </div>
+            </div>
           </div>
-          <p className=" font-inter font-normal leading-6.5 text-[#454652] text-[16px]">
-            {description}
-          </p>
+          <div className="flex font-inter flex-col gap-2 mb-8">
+            <div className="text-3xl font-bold text-[#191c1d] mb-10 flex items-baseline gap-2">
+              <span>${finalPrice.toFixed(2)}</span>
+              <span className="text-sm font-normal text-slate-400 line-through">
+                ${price.toFixed(2)}
+              </span>
+            </div>
+
+            <div className=" p-6 bg-[#f3f4f5] rounded-xl border border-indigo-50/50">
+              <div className="flex gap-4 items-start mb-4">
+                <MdOutlineDeliveryDining className="text-primary w-8 h-8" />
+
+                <div>
+                  <p className="text-sm font-bold">Complimentary Shipping</p>
+                  <p className="text-xs text-slate-500">
+                    {shippingInformation}
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-4 items-start">
+                <MdVerified className="text-primary w-8 h-8" />
+
+                <div>
+                  <p className="text-sm font-bold">{warrantyInformation}</p>
+                  <p className="text-xs text-slate-500">
+                    Every archive piece is certified for quality and longevity.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" flex flex-col gap-4 ">
+            <button className=" shadow-xl hover:shadow-2xl active:scale-95 transition-all w-114 bg-primary py-5 px-8 leading-7 tracking-[0.45px] rounded-lg font-manrope text-white font-bold text-[18px] ">
+              Add To Cart
+            </button>
+            <button className=" hover:bg-[#f3f4f5] transition-colors duration-200 flex items-center justify-center border-[#C5C5D4]/20 border font-manrope gap-3 w-114  py-5  leading-7 tracking-[0.45px] rounded-lg   font-bold text-[18px] ">
+              <CiHeart className=" w-5  text-[#191C1D]" />
+              <span className="font-bold text-[18px] text-[#191C1D] leading-7">
+                Wishlist
+              </span>
+            </button>
+          </div>
+
+          <div className=" mb-12  w-114 flex flex-col gap-8 pt-8">
+            <div className="">
+              <span className="  text-primary font-inter text-[14px] leading-5 tracking-[1.4px]">
+                DESCRIPTION
+              </span>
+            </div>
+            <p className=" font-inter font-normal leading-6.5 text-[#454652] text-[16px]">
+              {description}
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Product/StarRating.jsx
+++ src/pages/Product/StarRating.jsx
@@ -3,8 +3,8 @@ import { IoMdStarOutline } from "react-icons/io";
 import { MdOutlineStar, MdOutlineStarHalf } from "react-icons/md";
 
 const StarRating = ({ rating }) => {
-  const fullStars = Math.floor(rating?.rate);
-  const halfStar = rating?.rate % 1;
+  const fullStars = Math.floor(rating);
+  const halfStar = rating % 1;
 
   return (
     <div className=" flex items-center ">
@@ -22,7 +22,7 @@ const StarRating = ({ rating }) => {
         }
       })}
       <span className="text-[14px] font-inter font-semibold leading-5 pl-2 text-[#191C1D]">
-        {rating?.rate}
+        {rating}
       </span>
     </div>
   );
