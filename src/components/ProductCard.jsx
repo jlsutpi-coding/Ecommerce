6988e600ddbf4cd965315c33658c5edc0c57e85a
Jl Sut Pi <sutpi070@gmail.com>
2026-04-03 00:11:25 +0700

    feat(cart): implement addToCart logic with price calculations

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,46 +1,57 @@
 import { Link } from "react-router";
 import ProductPricing from "./ProductPricing";
 import { LiaShoppingBagSolid } from "react-icons/lia";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../features/cartSlice";
 
 const ProductCard = ({ product }) => {
   const { category, title, id, thumbnail } = product;
-  const cartItems = useSelector((state) => state.carts);
+  const { cartItems } = useSelector((state) => state.carts);
   console.log(cartItems);
+
+  const dispatch = useDispatch();
+  const btnAddToCart = () => {
+    console.log(product);
+    dispatch(addToCart(product));
+  };
   return (
     <Link className="" to={`/product/${id}`}>
-      <div className=" group  w-70.5 h-112.25 flex flex-col gap-5  rounded-lg  relative ">
-        <div className="w-full h-88.25 bg-[#F3F4F5]  overflow-hidden rounded-lg">
-          <img
-            src={thumbnail}
-            className="w-full group-hover:scale-105 transition-all duration-300 h-full object-contain"
-            alt=""
-          />
-          {product.discountPercentage > 10 && (
-            <span className="absolute top-2 left-2 z-20 bg-red-500 text-white px-2 py-1 text-xs rounded">
-              -{Math.round(product.discountPercentage)}%
-            </span>
-          )}
-        </div>
-        <div className=" p-2 flex flex-col gap-[4.5px] ">
-          <div className=" font-inter text-priamry tracking-[1px] leading-3.75 font-semibold text-[10px] opacity-60">
-            {category.toUpperCase()}
+      <div className="   w-70.5 h-112.25 flex flex-col gap-5  rounded-lg  relative ">
+        <div className="group w-full h-88.25">
+          <div className=" bg-[#F3F4F5]  overflow-hidden rounded-lg">
+            <img
+              src={thumbnail}
+              className="w-full group-hover:scale-105 transition-all duration-300 h-full object-contain"
+              alt=""
+            />
+            {product.discountPercentage > 10 && (
+              <span className="absolute top-2 left-2 z-20 bg-red-500 text-white px-2 py-1 text-xs rounded">
+                -{Math.round(product.discountPercentage)}%
+              </span>
+            )}
           </div>
-          <h2 className=" font-bold text-[18px] leading-7 text-[#191C1D] tracking-[0px]">
-            {title.length > 20 ? title.substring(0, 20) + "..." : title}
-          </h2>
-          <ProductPricing product={product} page={"home"} />
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-            className="mt-4 w-full flex items-center justify-center gap-2 bg-primary hover:bg-primary/90 text-white py-3 rounded-lg font-headline font-bold text-sm transition-all duration-300 group/btn active:scale-[0.98]"
-          >
-            <LiaShoppingBagSolid className="w-5 h-5" />
-
-            <span>Add to Cart</span>
-          </button>
+          <div className=" p-2 flex flex-col gap-[4.5px] ">
+            <div className=" font-inter text-priamry tracking-[1px] leading-3.75 font-semibold text-[10px] opacity-60">
+              {category.toUpperCase()}
+            </div>
+            <h2 className=" font-bold text-[18px] leading-7 text-[#191C1D] tracking-[0px]">
+              {title.length > 20 ? title.substring(0, 20) + "..." : title}
+            </h2>
+            <ProductPricing product={product} page={"home"} />
+          </div>
         </div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            btnAddToCart();
+          }}
+          className="mt-4 w-full flex items-center justify-center gap-2 bg-primary hover:bg-primary/90 text-white py-3 rounded-lg font-headline font-bold text-sm transition-all duration-300 group/btn active:scale-[0.98]"
+        >
+          <LiaShoppingBagSolid className="w-5 h-5" />
+
+          <span>Add to Cart</span>
+        </button>
       </div>
     </Link>
   );
--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -10,7 +10,54 @@ const initialState = {
 export const cartsSlice = createSlice({
   name: "carts",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, { payload }) => {
+      const existingItem = state.cartItems.find(
+        (item) => item.id === payload.id,
+      );
+
+      // Calculate totals for 1 unit (for new items) or N units (for existing item)
+      const newQuantity = !existingItem ? 1 : existingItem.quantity + 1;
+      const totals = calculateProductDetailTotal(payload, newQuantity);
+
+      if (!existingItem) {
+        state.cartItems.push({
+          id: payload.id,
+          title: payload.title,
+          price: payload.price,
+          quantity: 1,
+          thumbnail: payload.thumbnail,
+          discountPercentage: payload.discountPercentage,
+          total: totals.normalTotalPrice,
+          discountedTotal: totals.totalDiscountedPrice,
+        });
+      } else {
+        existingItem.quantity += 1;
+        existingItem.total = totals.normalTotalPrice;
+        existingItem.discountedTotal = totals.totalDiscountedPrice;
+      }
+    },
+  },
 });
 
+const calculateProductDetailTotal = (product, quantity = 1) => {
+  // 1. Convert to cents
+  const priceInCents = Math.round(product.price * 100);
+
+  // 2. Calculate discount per items in cents
+  const discountPerItemCents = Math.round(
+    priceInCents * (product.discountPercentage / 100),
+  );
+
+  // 3. Subtract discount from original price
+  const discountedPricePerItemCent = priceInCents - discountPerItemCents;
+
+  return {
+    normalTotalPrice: (priceInCents * quantity) / 100,
+    totalDiscountedPrice: (discountedPricePerItemCent * quantity) / 100,
+    totalSaving: (discountPerItemCents * quantity) / 100,
+  };
+};
+
 export default cartsSlice.reducer;
+export const { addToCart } = cartsSlice.actions;
