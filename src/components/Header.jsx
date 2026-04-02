6e86b2d82b230dcdc6a114d0e2fd1a64e3c014c9
Jl Sut Pi <sutpi070@gmail.com>
2026-04-03 00:24:10 +0700

    feat(header): display total cart items count in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router";
 
 import { LiaShoppingBagSolid } from "react-icons/lia";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.carts);
   return (
     <nav className=" px-8 fixed top-0 w-full z-50 bg-[#f8f9fa]/80  backdrop-blur-xl shadow-[0_4px_30px_rgba(0,0,0,0.05)]  h-21   flex justify-between items-center">
       <div>
@@ -16,8 +18,13 @@ const Header = () => {
         <div>
           <input type="text" placeholder="search" />
         </div>
-        <button className=" relative">
+        <button className=" cursor-pointer relative">
           <LiaShoppingBagSolid className="w-7 h-7" />
+          {cartItems.length ? (
+            <span className=" text-white text-[10px]  bg-primary rounded-full h-4 w-4 absolute top-0 r-0">
+              {cartItems.length}
+            </span>
+          ) : null}
         </button>
       </div>
     </nav>
