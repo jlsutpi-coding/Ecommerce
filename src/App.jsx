aaff7628ba8408e0018851e99ca8f6fc2c81f896
Jl Sut Pi <sutpi070@gmail.com>
2026-04-21 22:04:57 +0700

    feat(search): add SearchPage and fix state sync issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import HomePage from "./pages/Home/HomePage";
 import ProductPage from "./pages/Product/ProductPage";
 import CartPage from "./pages/cart/CartPage";
 import WatchlistPage from "./pages/watchlist/WatchlistPage";
+import SearchPage from "./pages/SearchPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products/:productId" element={<ProductPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/watchlist" element={<WatchlistPage />} />
+        <Route index element={<HomePage />} />
+        <Route path="products/:productId" element={<ProductPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="watchlist" element={<WatchlistPage />} />
+        <Route path="search" element={<SearchPage />} />
       </Route>
     </Routes>
   );
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../features/productSlice";
 
 const Layout = () => {
+  const { productsStatus, products } = useSelector((state) => state.products);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productsStatus === "idle") {
+      dispatch(fetchProducts());
+    }
+  }, [dispatch, productsStatus]);
+  if (productsStatus === "pending") return <>Products are pending</>;
+  if (productsStatus === "failed") return <>Products fetch is error</>;
+  if (productsStatus === "successed" && products.length === 0)
+    return <>No prodcuts available </>;
   return (
     <div className=" flex flex-col min-h-screen">
       <Header />
