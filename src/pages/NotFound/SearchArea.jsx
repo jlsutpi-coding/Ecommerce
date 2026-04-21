c9a0aa226b102eb79f4cfc30c68cfad66ea8c12e
Jl Sut Pi <sutpi070@gmail.com>
2026-04-21 22:28:24 +0700

    feat: add search functionality to 404 page

--- src/pages/NotFound/SearchArea.jsx
+++ src/pages/NotFound/SearchArea.jsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
+
+import { useNavigate } from "react-router";
+
+import { useDispatch } from "react-redux";
+import { setIsSearching, setSearchQery } from "../../features/searchSlice";
+
 import { CiSearch } from "react-icons/ci";
 
 const SearchArea = () => {
+  const dispatch = useDispatch();
+  const navigte = useNavigate();
+
+  const [inputValue, setInputValue] = useState("");
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setSearchQery(inputValue));
+    dispatch(setIsSearching(true));
+    if (inputValue.trim()) {
+      navigte(`/search?q=${encodeURIComponent(inputValue)}`);
+    }
+  };
   return (
     <div className="mx-auto">
-      <div className=" group relative  flex items-center transition-all duration-300">
+      <form
+        onSubmit={onSearchSubmit}
+        className=" group relative  flex items-center transition-all duration-300"
+      >
         <CiSearch className=" absolute left-4 text-[#757684] group-focus-within:text-primary transition-colors" />
         <input
           type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           className="pl-12 w-full border-none focus:ring-1 transition-all outline-none placeholder:text-[#757684]/60 rounded-lg focus:ring-primary bg-[#e1e3e4] focus:bg-white pr-6 py-4.5 font-inter font-normal text-[16px]"
           placeholder="Search the archive"
         />
-      </div>
+      </form>
     </div>
   );
 };
