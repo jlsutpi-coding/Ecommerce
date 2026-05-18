5b5b88b49257eeca4f9c30bf7d53ff4ae3acf5fd
Jl Sut Pi <sutpi070@gmail.com>
2026-05-18 23:38:20 +0700

    add: category section skeleton

---
+++ src/components/CategorySectionSkeleton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CategorySectionSkeleton = () => {
+  return (
+    <div className=" lg:w-64">
+      <div className=" w-[50%] h-2 "></div>
+    </div>
+  );
+};
+
+export default CategorySectionSkeleton;
