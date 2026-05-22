d70ed8460b389a2ebb292e887bf1301c0f5ac719
Jl Sut Pi <sutpi070@gmail.com>
2026-05-22 23:54:09 +0700

    fix: add missing useCallback import in CartPage

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+
 import RightColumn from "./RightColumn";
 import LeftColumn from "./LeftColumn";
 import DeleteModal from "./DeleteModal";
@@ -12,6 +14,7 @@ const CartPage = () => {
     setTargetId(id);
     setOpenModal(true);
   }, []);
+
   return (
     <main className="max-w-screen-2xl pt-22 lg:pt-32  px-4 md:px-6 lg:px-8 xl:px-12 bg-[#f8f9fa] dark:bg-[#0B1326] dark-transition pb-10 lg:pb-20 mx-auto">
       <DeleteModal
