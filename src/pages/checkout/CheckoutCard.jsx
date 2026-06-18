d37ffcb70a6f3399cc84ade638ce78906ad28a53
Jl Sut Pi <sutpi070@gmail.com>
2026-06-18 23:26:22 +0700

    feat: implement CheckoutCard for cart items in checkout

---
+++ src/pages/checkout/CheckoutCard.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router";
+
+import PriceDisplay from "../cart/PriceDisplay";
+import ImageUrl from "../../components/ImageUrl";
+
+const CheckoutCard = ({ cart }) => {
+  const {
+    id,
+    title,
+    thumbnail,
+    category,
+    discountPercentage,
+    discountedTotal,
+    price,
+    quantity,
+  } = cart;
+
+  return (
+    <div className="flex md:gap-6 lg:gap-8 dark-transition shadow-sm p-3 md:p-4 lg:p-6">
+      <Link to={`/products/${id}`} className="shrink-0">
+        <ImageUrl
+          src={thumbnail}
+          alt={title}
+          containerClasses="w-40 rounded-lg relative h-35 overflow-hidden"
+        />
+      </Link>
+
+      <div className=" grow flex flex-col   py-2">
+        <div className="w-full flex justify-between items-start">
+          <div>
+            <h3 className="text-lg lg:text-xl font-bold mb-1 font-manrope dark-transition dark:text-[#DAE2FD]">
+              {title}
+            </h3>
+            <p className="font-inter dark-transition dark:text-[#FFB695] ">
+              {category}
+            </p>
+            <p className="font-inter dark-transition mt-2 dark:text-[#DAE2FD] ">
+              Quantity : {quantity}
+            </p>
+          </div>
+        </div>
+        <div className="mt-2 lg:mt-4 flex flex-row items-center justify-between">
+          <span className=" bg-[#3F51B5] rounded-full font-inter text-[#CACFFF] text-[10px] lg:text-xs  lg:leading-4 py-1 px-2 lg:px-3">
+            {discountPercentage} %OFF
+          </span>
+          <PriceDisplay discountedTotal={discountedTotal} price={price} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutCard;
