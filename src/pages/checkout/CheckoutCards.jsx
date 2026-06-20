c640642d96637c5db1a74c10b4edb6698940a88e
Jl Sut Pi <sutpi070@gmail.com>
2026-06-20 21:08:34 +0700

    fix(checkout): correct subtotal, discount, and total calculations using Redux state

--- src/pages/checkout/CheckoutCards.jsx
+++ src/pages/checkout/CheckoutCards.jsx
@@ -2,16 +2,20 @@ import { useSelector } from "react-redux";
 import CheckoutCard from "./CheckoutCard";
 
 const CheckoutCards = () => {
-  const { cartItems, totalDiscountedPrice } = useSelector(
-    (state) => state.cart,
-  );
-  const total = totalDiscountedPrice ?? 0;
-  const sameKeyClass =
+  const { cartItems, totalDiscountedPrice, totalAmount, totalDiscount } =
+    useSelector((state) => state.cart);
+  const subTotalPrice = totalAmount ?? 0;
+  const totalPrice = totalDiscountedPrice ?? 0;
+  const totalDiscountPrice = totalDiscount ?? 0;
+  const labelClass =
     "font-normal text-sm lg:text-base leading-6 font-inter text-[#454652] dark-transition dark:text-[#C7C4D8]";
 
+  const valueClasses =
+    "dark:text-[#FFB695] text-[#2E7D32]  font-normal text-sm lg:text-base leading-6 font-inter dark-transition";
+
   return (
-    <div className="rounded-xl mb-10 bg-[#f8f9fa] dark:bg-[#0B1326] overflow-hidden">
-      <div role="list">
+    <div className="rounded-xl mb-10 bg-[#f8f9fa] dark:bg-[#222a3d]  overflow-hidden">
+      <div role="list" className="[&>*:first-child]:rounded-t-xl ">
         {cartItems?.length === 0 ? (
           <p className="p-4 text-center">Your cart is empty.</p>
         ) : (
@@ -20,20 +24,25 @@ const CheckoutCards = () => {
       </div>
       <div className=" flex flex-col p-3 md:p-4 lg:p-6 gap-4">
         <div className="flex justify-between">
-          <p className={sameKeyClass}>Subtotal</p>
-          <p className={sameKeyClass}> ${total.toFixed(2)}</p>
+          <p className={labelClass}>Subtotal</p>
+          <p className={labelClass}> ${subTotalPrice.toFixed(2)}</p>
         </div>
         <div className="flex justify-between">
-          <p className={sameKeyClass}>Shipping</p>
-          <p className={sameKeyClass}>Complimentary</p>
+          <p className={labelClass}>Discount</p>
+          <p className={valueClasses}> - ${totalDiscountPrice.toFixed(2)}</p>
         </div>
+        <div className="flex justify-between">
+          <p className={labelClass}>Shipping</p>
+          <p className={valueClasses}>Complimentary</p>
+        </div>
+
         <div className="h-px bg-[#464555]/20 my-2"></div>
         <div className="flex justify-between items-baseline">
           <p className="text-base lg:text-lg text-[#191C1D] dark-transition dark:text-[#DAE2FD] font-bold font-manrope leading-7">
             Total
           </p>
           <p className="text-xl lg:text-2xl text-primary dark-transition dark:text-[#DAE2FD] font-semibold font-inter leading-8">
-            ${total.toFixed(2)}
+            ${totalPrice.toFixed(2)}
           </p>
         </div>
       </div>
