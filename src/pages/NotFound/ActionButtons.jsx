705bcb73cb5c7503492de491fe5648c9d08f30ba
Jl Sut Pi <sutpi070@gmail.com>
2026-06-23 23:32:52 +0700

    feat: add responsive not-found page

--- src/pages/NotFound/ActionButtons.jsx
+++ src/pages/NotFound/ActionButtons.jsx
@@ -1,20 +1,19 @@
-import React from "react";
-import BtnPrimary from "../../components/BtnPrimary";
 import { Link } from "react-router";
+
+import BtnPrimary from "../../components/BtnPrimary";
 
 const ActionButtons = () => {
   return (
     <div className=" flex justify-center items-center gap-6">
       <Link to={"/"}>
-        <BtnPrimary additional={"px-10 py-4"}>
-          <span className=" ">Back to Home</span>
-        </BtnPrimary>
+        <BtnPrimary additional={"px-10 py-4"}>Back to Home</BtnPrimary>
       </Link>
       <Link to={"/"}>
         <BtnPrimary
           bg_color={"bg-[#C5C5D4]/20"}
           hover=" hover:bg-[#f3f4f5] dark:hover:bg-slate-800"
-          additional="shadow-lg border border-[#C5C5D4]/20 dark:border-white/10"
+          text_color="text-primary dark:text-white"
+          additional="shadow-lg border px-10 py-4 border-[#C5C5D4]/20 dark:border-white/10"
         >
           Browse New Arrivals
         </BtnPrimary>
--- src/pages/NotFound/TypoContent.jsx
+++ src/pages/NotFound/TypoContent.jsx
@@ -1,7 +1,7 @@
 const TypoContent = () => {
   return (
     <div className="flex flex-col gap-6 items-center">
-      <h2 className="font-manrope text-5xl text-[#191C1D] font-extrabold leading-12 tracking-[-1.2px]">
+      <h2 className="font-manrope  dark:text-[#DAE2FD] text-[#191C1D] dark-transition text-3xl md:text-4xl xl:text-5xl font-extrabold leading-12 tracking-[-1.2px]">
         Lost in the Archive?
       </h2>
       <p className="text-center font-normal font-inter text-lg text-[#565C84]/80 leading-7.25">
